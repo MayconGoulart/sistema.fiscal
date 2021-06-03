fcd3990d5faeec106e221e8ac72f9b2bfda99079
Emerson Santos <emersonsantos@unifei.edu.br>
2021-06-03 16:54:01 -0300

    imp/menu-tela-home-parceiros
    Implementação do menu vertical

--- src/components/template/Nav.jsx
+++ src/components/template/Nav.jsx
@@ -2,28 +2,33 @@ import './Nav.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+
 export default props =>
     <aside className="menu-area">
         <nav className="menu">
-            {/* Refatorar em casa! */}
+
             <Link to="/">
+                <i className="fa fa-home"></i> Home
+            </Link>
+
+            <Link to="/conta">
                 <i className="fa fa-user-o"></i> Minha Conta
             </Link>
 
-            <Link to="/">
+            <Link to="/fiscal">
                 <i className="fa fa-file-text-o"></i> Fiscal
             </Link>
 
-            <Link to="/">
+            <Link to="/inventario">
                 <i className="fa fa-archive"></i> Inventário
             </Link>
 
-            <Link to="/">
+            <Link to="/parceiros" >
                 <i className="fa fa-users"></i> Parceiros
             </Link>
 
             <Link to="/">
-                <i className="fa fa-line-chart"></i> Contabilidadde
+                <i className="fa fa-line-chart"></i> Contabilidade
             </Link>
         </nav>
     </aside>
