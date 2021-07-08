df71d5924d3136182d4d110e7226cd4ec64a7eac
Ygor Carvalho <ygcarvalh@gmail.com>
2021-07-08 16:16:07 -0300

    [FIX] Cabeçalho

--- sistema-principal/Client/src/components/Principal/Cabecalho.jsx
+++ sistema-principal/Client/src/components/Principal/Cabecalho.jsx
@@ -15,13 +15,14 @@ function Cabecalho(){
 
         <div>
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand herf="#home">NOME DA EMPRESA</Navbar.Brand>
-                <Avatar alt="Remy Sharp" src="">B</Avatar>
+                <Navbar.Brand herf="#home">Auto Peças Goulart</Navbar.Brand>
                 <Nav.Link href="/">Home</Nav.Link>
                 <Nav.Link href="/parceiros">Parceiros</Nav.Link>
                 <Nav.Link href="/inventario">Inventário</Nav.Link>
                 <Nav.Link href="/fiscal">Fiscal</Nav.Link>
                 <Nav.Link href="/contabilidade">Contabilidade</Nav.Link>
+                <Nav.Link href="/configuracoes">Configurações</Nav.Link>
+                <Avatar alt="Remy Sharp" src="">G4</Avatar>
             </Navbar>
         </div>
         
