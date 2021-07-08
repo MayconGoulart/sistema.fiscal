85c80c6fb2a2b9dccdc79cf52771cf0b6bdd8d01
Isa <souza.isa96@gmail.com>
2021-07-08 17:02:29 -0300

    [WIP] Estilização Parceiros

--- sistema-principal/Client/src/components/Parceiros/CadastroParceiro.jsx
+++ sistema-principal/Client/src/components/Parceiros/CadastroParceiro.jsx
@@ -109,14 +109,24 @@ function CadastroParceiro() {
     return (
         <>
 
-            <Container className="md container-generico">
+            <section class="infos-card"> 
 
                 <form onSubmit={handleSubmit}>
 
-                    <div>
-                        <Person style={{ fontSize: 50 }} />
-                        <FormControl>
-                            <RadioGroup>
+                    <div class="info-inicial">
+                        <div class="icon-inicial">
+                             <Person style={{ fontSize: 80 }} />
+                        </div>
+                       
+                       <div> 
+                           <div>
+                               <TextField id="nome" label="Nome" onChange={(event) => {
+                                    setNome(event.target.value);
+                                }} />
+                           </div>
+                           <div>
+                               <FormControl>
+                                <RadioGroup>
                                 <FormControlLabel id={data.pessoaFisica} value={data.pessoaFisica}
                                     checked={data.pessoaFisica === tipoParceiro} onChange={setTipoParceiro}
                                     control={<Radio />} label="Pessoa Física" />
@@ -124,114 +134,128 @@ function CadastroParceiro() {
                                 <FormControlLabel id={data.pessoaJuridica} value={data.pessoaJuridica}
                                     checked={data.pessoaJuridica === tipoParceiro} onChange={setTipoParceiro}
                                     control={<Radio />} label="Pessoa Jurídica" />
-                            </RadioGroup>
-                        </FormControl>
-                        <TextField id="nome" label="Nome" onChange={(event) => {
-                            setNome(event.target.value);
-                        }} />
-                        {tipoParceiro === "pessoaFisica" &&
-                            <div>
-                                <TextField id="cpf" label="CPF" onChange={(event) => {
-                                    setCpf(event.target.value);
-                                }}/>
-                                <TextField id="rg" label="RG" onChange={(event) => {
-                                    setRg(event.target.value);
-                                }}/>
-                            </div>
-                        }
-
-                        {tipoParceiro === "pessoaJuridica" &&
-                            <div>
-                                <TextField id="cnpj" label="CNPJ" onChange={(event) => {
-                                    setCnpj(event.target.value);
-                                }} />
-                            </div>
-                        }
-
+                                </RadioGroup>
+                                </FormControl>
+                           </div> 
+                       </div>
                     </div>
 
                     {tipoParceiro === "pessoaFisica" &&
 
-                        <div>
-                            <h2>Informações (Pessoa Física)</h2>
-                            <TextField id="telefone" label="Telefone" onChange={(event) => {
-                                    setTelefone(event.target.value);
-                                }} />
-                            <TextField id="celular" label="Celular" onChange={(event) => {
-                                    setCelular(event.target.value);
-                                }}/>
-                            <TextField id="email" label="E-mail" onChange={(event) => {
-                                    setEmail(event.target.value);
-                                }}/>
-                            <TextField id="website" label="Website" onChange={(event) => {
-                                    setWebsite(event.target.value);
-                                }}/>
-                            <TextField id="cargo" label="Cargo" onChange={(event) => {
-                                    setCargo(event.target.value);
-                                }}/>
-                            <TextField id="empresa" label="Empresa" onChange={(event) => {
-                                    setEmpresa(event.target.value);
-                                }}/>
+                        <div class="info-gerais">
+                            <h2 class="titulo-info-gerais">Informações (Pessoa Física)</h2>
+                            <div class="info-gerais-conteudo">
+                                <div class="info-gerais-1">
+                                    <TextField id="cpf" label="CPF" onChange={(event) => {
+                                        setCpf(event.target.value);
+                                    }}/>
+                                    <TextField id="rg" label="RG" onChange={(event) => {
+                                        setRg(event.target.value);
+                                    }}/>
+                                    <TextField id="cargo" label="Cargo" onChange={(event) => {
+                                        setCargo(event.target.value);
+                                    }}/>
+                                    <TextField id="telefone" label="Telefone" onChange={(event) => {
+                                        setTelefone(event.target.value);
+                                    }} />
+                                </div>
+                                
+                                <div class="info-gerais-2">
+                                    <TextField id="celular" label="Celular" onChange={(event) => {
+                                        setCelular(event.target.value);
+                                    }}/>
+                                    <TextField id="email" label="E-mail" onChange={(event) => {
+                                        setEmail(event.target.value);
+                                    }}/>
+                                    <TextField id="website" label="Website" onChange={(event) => {
+                                        setWebsite(event.target.value);
+                                    }}/>
+                                </div>
+                            </div>
+                            
                         </div>
                     }
 
                     {tipoParceiro === "pessoaJuridica" &&
 
-                        <div>
-                            <h2>Informações (Pessoa Jurídica)</h2>
-                            <TextField id="nome-legal" label="Nome Legal" onChange={(event) => {
+                        <div class="info-gerais">
+                            <h2 class="titulo-info-gerais">Informações (Pessoa Jurídica)</h2>
+                            <div class="info-gerais-conteudo">
+                                <div class="info-gerais-1">
+                                    <TextField id="nome-legal" label="Nome Legal" onChange={(event) => {
                                     setNomeLegal(event.target.value);
-                                }}/>
-                            <TextField id="inscricao-estadual" label="Inscrição Estadual" onChange={(event) => {
-                                    setIe(event.target.value);
-                                }}/>
-                            <TextField id="telefone" label="Telefone" onChange={(event) => {
-                                    setTelefone(event.target.value);
-                                }}/>
-                            <TextField id="celular" label="Celular" onChange={(event) => {
+                                        }}/>
+                                    <TextField id="cnpj" label="CNPJ" onChange={(event) => {
+                                            setCnpj(event.target.value);
+                                        }} />      
+                                    <TextField id="inscricao-estadual" label="Inscrição Estadual" onChange={(event) => {
+                                            setIe(event.target.value);
+                                        }}/>
+                                    <TextField id="telefone" label="Telefone" onChange={(event) => {
+                                            setTelefone(event.target.value);
+                                        }}/>
+                                    </div>
+                                <div class="info-gerais-2">
+                                     <TextField id="celular" label="Celular" onChange={(event) => {
                                     setCelular(event.target.value);
-                                }}/>
-                            <TextField id="email" label="E-mail" onChange={(event) => {
-                                    setEmail(event.target.value);
-                                }}/>
-                            <TextField id="website" label="Website" onChange={(event) => {
-                                    setWebsite(event.target.value);
-                                }}/>
+                                        }}/>
+                                    <TextField id="email" label="E-mail" onChange={(event) => {
+                                            setEmail(event.target.value);
+                                        }}/>
+                                    <TextField id="empresa" label="Empresa" onChange={(event) => {
+                                            setEmpresa(event.target.value);
+                                        }}/>
+                                    <TextField id="website" label="Website" onChange={(event) => {
+                                            setWebsite(event.target.value);
+                                        }}/>
+                                </div>
+                            </div>
                         </div>
                     }
 
-                    <div>
-                        <h2>Endereço</h2>
-                        <TextField id="cep" label="CEP" onChange={(event) => {
-                                    setCep(event.target.value);
-                                }}/><Search />
-                        <TextField id="rua" label="Rua" onChange={(event) => {
-                                    setRua(event.target.value);
-                                }}/>
-                        <TextField id="numero" label="Número" onChange={(event) => {
-                                    setNumero(event.target.value);
-                                }}/>
-                        <TextField id="complemento" label="Complemento" onChange={(event) => {
-                                    setComplemento(event.target.value);
-                                }}/>
-                        <TextField id="distrito" label="Distrito" onChange={(event) => {
-                                    setDistrito(event.target.value);
-                                }}/>
-                        <TextField id="cidade" label="Cidade" onChange={(event) => {
-                                    setCidade(event.target.value);
-                                }}/>
-                        <TextField id="estado" label="Estado" onChange={(event) => {
-                                    setEstado(event.target.value);
-                                }}/>
-                        <TextField id="pais" label="País" onChange={(event) => {
-                                    setPais(event.target.value);
-                                }}/>
+                    <div class="info-gerais">
+                        <h2 class="titulo-info-gerais">Endereço</h2>
+                        <div class="info-gerais-conteudo">
+                            <div class="info-gerais-1">
+                                <div>
+                                    <TextField id="cep" label="CEP" onChange={(event) => {
+                                            setCep(event.target.value);
+                                        }}/><Search />
+                                </div>
+
+                                <TextField id="rua" label="Rua" onChange={(event) => {
+                                            setRua(event.target.value);
+                                        }}/>
+                                <TextField id="numero" label="Número" onChange={(event) => {
+                                            setNumero(event.target.value);
+                                        }}/>
+                                <TextField id="complemento" label="Complemento" onChange={(event) => {
+                                            setComplemento(event.target.value);
+                                        }}/>
+                            </div>
+                            <div class="info-gerais-2">
+                                <TextField id="distrito" label="Distrito" onChange={(event) => {
+                                        setDistrito(event.target.value);
+                                        }}/>
+                                <TextField id="cidade" label="Cidade" onChange={(event) => {
+                                            setCidade(event.target.value);
+                                        }}/>
+                                <TextField id="estado" label="Estado" onChange={(event) => {
+                                            setEstado(event.target.value);
+                                        }}/>
+                                <TextField id="pais" label="País" onChange={(event) => {
+                                            setPais(event.target.value);
+                                        }}/>
+                            </div>
+                        </div>
+
+                       
                     </div>
 
-                    <div>
-                        <h2>Fiscal</h2>
+                    <div class="info-gerais">
                         {tipoParceiro === "pessoaFisica" &&
                             <div>
+                                <h2 class="titulo-info-gerais">Fiscal</h2>
                                 <FormControl>
                                     <FormLabel component="legend">Perfil Fiscal</FormLabel>
                                     <RadioGroup onChange={(event) => {setPerfilFiscal(event.target.value);}} >
@@ -245,42 +269,48 @@ function CadastroParceiro() {
 
                         {tipoParceiro === "pessoaJuridica" &&
                             <div>
+                                <h2 class="titulo-info-gerais">Fiscal</h2>
                                 <TextField id="inscricao-municipal" label="Inscrição Municipal" />
-                                <FormControl>
-                                    <FormLabel component="legend">Perfil Fiscal (Jurídico)</FormLabel>
-                                    <RadioGroup onChange={(event) => {setPerfilFiscal(event.target.value);}}>
-                                        <FormControlLabel value="contribuinte-sn" control={<Radio />} label="Contribuinte Simples Nacional" />
-                                        <FormControlLabel value="nao-contribuinte-sn" control={<Radio />} label="Simples Nacional Não Contribuinte" />
-                                        <FormControlLabel value="isento-sn" control={<Radio />} label="Simples Nacional Isento" />
-                                        <FormControlLabel value="contribuinte" control={<Radio />} label="Contribuinte" />
-                                        <FormControlLabel value="nao-contribuinte" control={<Radio />} label="Não Contribuinte" />
-                                        <FormControlLabel value="isento" control={<Radio />} label="Isento" />
-                                    </RadioGroup>
-                                </FormControl>
-                                <FormControl>
-                                    <FormLabel component="legend">Tax Framework</FormLabel>
-                                    <RadioGroup onChange={(event) => {setTx(event.target.value);}}>
-                                        <FormControlLabel value="simples-nacional" control={<Radio />} label="Simples Nacional" />
-                                        <FormControlLabel value="lucro-real" control={<Radio />} label="Lucro Real" />
-                                        <FormControlLabel value="lucro-presumido" control={<Radio />} label="Lucro Presumido" />
-                                    </RadioGroup>
-                                </FormControl>
+                                <div class="perfil-taxas info-gerais-conteudo">
+                                    <div>
+                                        <FormControl>
+                                        <FormLabel component="legend">Perfil Fiscal (Jurídico)</FormLabel>
+                                        <RadioGroup onChange={(event) => {setPerfilFiscal(event.target.value);}}>
+                                            <FormControlLabel value="contribuinte-sn" control={<Radio />} label="Contribuinte Simples Nacional" />
+                                            <FormControlLabel value="nao-contribuinte-sn" control={<Radio />} label="Simples Nacional Não Contribuinte" />
+                                            <FormControlLabel value="isento-sn" control={<Radio />} label="Simples Nacional Isento" />
+                                            <FormControlLabel value="contribuinte" control={<Radio />} label="Contribuinte" />
+                                            <FormControlLabel value="nao-contribuinte" control={<Radio />} label="Não Contribuinte" />
+                                            <FormControlLabel value="isento" control={<Radio />} label="Isento" />
+                                        </RadioGroup>
+                                        </FormControl>
+                                    </div>
+                                    <div>
+                                         <FormControl>
+                                        <FormLabel component="legend">Tax Framework</FormLabel>
+                                        <RadioGroup onChange={(event) => {setTx(event.target.value);}}>
+                                            <FormControlLabel value="simples-nacional" control={<Radio />} label="Simples Nacional" />
+                                            <FormControlLabel value="lucro-real" control={<Radio />} label="Lucro Real" />
+                                            <FormControlLabel value="lucro-presumido" control={<Radio />} label="Lucro Presumido" />
+                                        </RadioGroup>
+                                    </FormControl>
+                                    </div>
+                                   
+                                </div>
+                                
                             </div>
                         }
 
                     </div>
 
-                    <div>
-                        <Button onClick={addContato} variant="success">
-                            Salvar
-                            <Link to="/parceiros" />
-                        </Button>
+                    <div class="btn-salvar">
+                        <Button onClick={addContato} variant="success">Salvar</Button>
                         <Button variant="danger">Cancelar</Button>
                     </div>
 
                 </form>
 
-            </Container>
+            </section>
 
 
         </>
